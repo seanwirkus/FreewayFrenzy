fa01cc24b70822eeaf9305b23ef6ec53241f00fc
seanwirkus <123360930+seanwirkus@users.noreply.github.com>
2026-04-02 16:17:26 -0700

    v1

--- controller/build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/Users/sean/esp/v5.5.1/esp-idf/components/mbedtls/mbedtls/library/error.c
--- controller/build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/Users/sean/esp/v5.5.1/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
